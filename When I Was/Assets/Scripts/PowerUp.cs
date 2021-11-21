8116782b3a919e85a4142f64b65c4d208d8d62d7
Charles <charles.sayamath@gmail.com>
2021-11-21 01:02:02 +0100

    Power up

---
+++ When I Was/Assets/Scripts/PowerUp.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PowerUp : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            other.GetComponent<PlayerController>().powerUp();
+            GetComponent<Collider2D>().enabled = false;
+        }
+    }
+}
