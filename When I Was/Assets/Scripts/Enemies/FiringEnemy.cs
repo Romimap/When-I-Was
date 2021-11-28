3e9ed3fb51f54cd9521b96a2a2266a9a99cf3650
Emery Bourget-Vecchio <emery.vecchio@gmail.com>
2021-11-28 13:29:17 +0100

    Niveau facile

--- When I Was/Assets/Scripts/Enemies/FiringEnemy.cs
+++ When I Was/Assets/Scripts/Enemies/FiringEnemy.cs
@@ -8,19 +8,29 @@ public class FiringEnemy : MonoBehaviour
 
     public float RoF = 0.5f;
     public GameObject weaponPrefab;
-
-
+    public float minAngle = 0.0f;
+    public float maxAngle = 360.0f;
+    public float rotSpeed = 1.0f;
+    private float currentAngle;
+    private int rotDir = 1; 
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentAngle = minAngle;
     }
 
     // Update is called once per frame
     void Update()
     {
         delay += Time.deltaTime;
+        currentAngle += Time.deltaTime * rotSpeed * rotDir;
 
+        if(currentAngle >= maxAngle)
+            rotDir = -1;
+        if(currentAngle <= minAngle)
+            rotDir = 1;
+        
+        this.transform.rotation = Quaternion.Euler(0.0f,0.0f,currentAngle);
         if (delay > (1 / RoF))
         {
             delay = 0;
--- When I Was/Assets/Scripts/Enemies/LaserFade.cs
+++ When I Was/Assets/Scripts/Enemies/LaserFade.cs
@@ -6,21 +6,24 @@ public class LaserFade : MonoBehaviour
 {
     public float velocity = 2;
 
+    private float timer = 0.0f;
     // Start is called before the first frame update
     void Start()
     {
         
     }
-
+    
     // Update is called once per frame
     void Update()
     {
+        timer += Time.deltaTime;
+        
         this.gameObject.transform.Translate(Vector3.right * Time.deltaTime * velocity);
     }
 
     protected void OnTriggerEnter2D(Collider2D collision)
     {
-        if(!collision.CompareTag("Danger"))
+        if(!collision.CompareTag("Danger") && timer > 0.5f)
             Destroy(this.gameObject);
     }
 }
